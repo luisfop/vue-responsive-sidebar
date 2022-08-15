e0edb246012169161eec63340e149d114324db48
luisop <luisfop77@gmail.com>
2022-08-15 19:41:06 -0300

    feat: started the sidebar styling

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,30 @@
+<template>
+  <aside>
+      SIDEBAR
+  </aside>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+aside{
+  display: flex;
+  flex-direction: column;
+  width: var(--sidebar-width);
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 1rem;
+  transition: 0.2 ease-out ;
+
+  background-color: var(--dark);
+  color: var(--light);
+
+  @media(max-width: 768px){
+    position: fixed;
+    z-index: 99;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,24 +1,7 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import router from './router/index';
 
-import { createRouter, createWebHistory } from "vue-router";
-
-import Home from "./views/Home.vue";
-import About from "./views/About.vue";
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: "/",
-      component: () => Home,
-    },
-    {
-      path: "/about",
-      component: () => About,
-    },
-  ],
-});
 
 createApp(App).use(router).mount("#app");
